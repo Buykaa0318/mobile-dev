f7ba3ed883bda8c6e569b03b8c3bf1fe12e4de60
davaanym8832 <143776136+davaanym8832@users.noreply.github.com>
2026-02-03 11:54:57 +0800

    item done

--- babyapp/app/index.tsx
+++ babyapp/app/index.tsx
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Link } from 'expo-router';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>babyapp</Text>
+      <Link 
+      href = {{
+        pathname: '\Item',
+        params:{cid:1, sid:2, iid:3},
+      }} >ITEM</Link>
       <StatusBar style="auto" />
     </View>
   );
